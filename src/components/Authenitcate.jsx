b9bbfdc350d1b77e0adde8e207872589dff82be8
Raven Hendricks <ravenkbeats@gmail.com>
2024-03-04 14:49:03 -0500

    added minimal data to test functionality of component

--- src/components/Authenitcate.jsx
+++ src/components/Authenitcate.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function Authenticate(){
-    return <h2>Authenticate!</h2>
+export default function Authenticate({ token }){
+    async function handleClick(){
+        console.log("Clicked!");
+    }
+    return <>
+        <h2>Authenticate!</h2>
+        <button onClick={handleClick}>
+            Authenticate Token
+        </button>
+    </>
 }
