d2ba46ea6972eb578c6342c9463d695b7c1295ed
Raven Hendricks <ravenkbeats@gmail.com>
2024-03-04 14:27:00 -0500

    imported usestate and created variables for the token and passed these states and functions into my signup and authenticate components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import './App.css'
-import Authenticate from './components/Authenitcate'
-import SignUpForm from './components/SignUpForm'
+import './App.css';
+import { useState } from 'react';
+import Authenticate from './components/Authenitcate';
+import SignUpForm from './components/SignUpForm';
 
 function App() {
-
+  const [token, setToken] = useState(null);
 
   return (
     <>
-    <SignUpForm />
-    <Authenticate />
+      <SignUpForm token={token} setToken={setToken} />
+      <Authenticate token ={token} setToken={setToken} />
     </>
   );
 }
