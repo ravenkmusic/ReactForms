a6f8d9705701e728f19f1911905e42f73d12a297
Raven Hendricks <ravenkbeats@gmail.com>
2024-03-04 13:35:43 -0500

    created simple form

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { useState } from "react";
 
 export default function SignUpForm(){
-    return <h2>Sign Up!</h2>;
+    const [ username, setUserName ] = useState("");
+    const [ password, setPassword ] = useState("");
+    const [ error, setError ] = useState(null);
+
+    return <div>
+        <h2>Sign Up!</h2>
+            <form>
+                <label>
+                    Username: <input />
+                </label>
+                <label>
+                    Password: <input />Password
+                </label>
+                <button>Submit</button>
+            </form>
+    </div>
 }
