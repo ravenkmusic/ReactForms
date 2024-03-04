45a475bfd0c7814546daa7ebcb2824c811e7cd16
Raven Hendricks <ravenkbeats@gmail.com>
2024-03-04 15:19:17 -0500

    added logic to handleclick function

--- src/components/Authenitcate.jsx
+++ src/components/Authenitcate.jsx
@@ -4,10 +4,19 @@ import { useState } from "react";
 export default function Authenticate({ token }){
     const [error, setError] = useState(null);
     const [successMessage, setSuccessMessage] = useState(null);
+    const authenticationAPIURL = `https://fsa-jwt-practice.herokuapp.com/authenticate`;
 
     async function handleClick(){
         try {
-            
+            const response = await fetch(`${authenticationAPIURL}`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                }
+            });
+            const result = response.json();
+            console.log(result);
         } catch (error) {
             setError(error.message);
         }
