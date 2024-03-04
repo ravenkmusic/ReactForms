6701d368ac4263de5839147aa00ac0cd0c57da57
Raven Hendricks <ravenkbeats@gmail.com>
2024-03-04 15:08:20 -0500

    added states to set token retrieved from API added button for authentication of token

--- src/components/Authenitcate.jsx
+++ src/components/Authenitcate.jsx
@@ -1,13 +1,26 @@
 import React from "react";
+import { useState } from "react";
 
 export default function Authenticate({ token }){
+    const [error, setError] = useState(null);
+    const [successMessage, setSuccessMessage] = useState(null);
+
     async function handleClick(){
+        try {
+            
+        } catch (error) {
+            setError(error.message);
+        }
         console.log("Clicked!");
     }
-    return <>
-        <h2>Authenticate!</h2>
-        <button onClick={handleClick}>
-            Authenticate Token
-        </button>
-    </>
+    return( 
+        <div>
+            <h2>Authenticate!</h2>
+            {successMessage && <p>{successMessage}</p>}
+            {error && <p>{error}</p>}
+            <button onClick={handleClick}>
+                Authenticate Token!
+            </button>
+        </div>
+    );
 }
